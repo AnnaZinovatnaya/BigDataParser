ea1ee0db27dfb2f2d93d42e67ffa81db2c1d1a0c
AnnaZinovatnaya <zinovatnaya@gmail.com>
2017-10-22 18:34:35 +0300

    Finished parser

--- src/main/java/parser/JsonParser.java
+++ src/main/java/parser/JsonParser.java
@@ -1,50 +1,51 @@
 package parser;
 
 import org.hibernate.HibernateException;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import parser.entities.Category;
-import parser.entities.Page;
-
+import org.json.*;
+import parser.entities.*;
 import java.io.*;
-import java.net.URL;
+import java.net.*;
 import java.nio.charset.Charset;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class JsonParser {
     public static void main (String[] args) throws IOException {
+        parseDataToDatabase();
 
-        System.out.println("Parsing categories...");
-        //List<Category> categories = parseCategoriesToDatabase();
-        System.out.println("Finished parsing categories!");
-
-        System.out.println("Saving categories to file...");
-        //WikiDB.getInstance().saveAllCategoriesToFile("D:\\BigData\\Categories.txt");
-        System.out.println("Categories are saved to file Categories.txt");
-
-        System.out.println("Parsing pages...");
-        //parsePagesToDatabase(categories);
-        System.out.println("Finished parsing pages!");
-
-        System.out.println("Saving pages to file...");
-        //WikiDB.getInstance().saveAllPagesToFile("D:\\BigData\\Pages.txt");
-        System.out.println("Pages are saved to file Pages.txt");
-
-        System.out.println("Saving all data to file...");
-        WikiDB.getInstance().saveAllDataToFile("D:\\BigData\\AllData.txt");
-        System.out.println("All data is saved to file AllData.txt");
+        saveDataToFiles();
 
         System.out.println("FINISH");
 
         return;
     }
 
-    public static List<Category> parseCategoriesToDatabase() throws IOException {
-        List<Category> categoriesResult = new ArrayList<Category>();
+    public static void saveDataToFiles() throws IOException {
+        System.out.println("Saving categories to file...");
+        WikiDB.getInstance().saveAllCategoriesToFile("D:\\BigData\\Categories.txt");
+        System.out.println("Categories are saved to file Categories.txt");
+
+        System.out.println("Saving pages to file...");
+        WikiDB.getInstance().saveAllPagesToFile("D:\\BigData\\Pages.txt");
+        System.out.println("Pages are saved to file Pages.txt");
+
+        System.out.println("Saving all data to file...");
+        WikiDB.getInstance().saveAllDataToFile("D:\\BigData\\AllData.txt");
+        System.out.println("All data is saved to file AllData.txt");
+    }
+
+    public static void parseDataToDatabase() throws IOException {
+        System.out.println("Parsing categories...");
+        parseCategoriesToDatabase();
+        System.out.println("Finished parsing categories!");
+
+        List<Category> categories = WikiDB.getAllCategories();
+
+        System.out.println("Parsing pages...");
+        parsePagesToDatabase(categories);
+        System.out.println("Finished parsing pages!");
+    }
+
+    public static void parseCategoriesToDatabase() throws IOException {
         int counter=1;
         for (char letter = 'A'; letter <= 'Z'; ++letter) {
             String url = "https://en.wikipedia.org/w/api.php?action=query&acprop=size&format=json&list=allcategories&aclimit=max&acprefix=" + letter;
@@ -56,16 +57,13 @@ public class JsonParser {
             Iterator<Object> iterator = categories.iterator();
 
             while (iterator.hasNext()) {
-                System.out.println(counter++);
+                System.out.println("Category - " + counter++ + "/13000");
                 JSONObject categoryJSON = (JSONObject) iterator.next();
 
                 Category category = new Category(categoryJSON.get("*").toString(), Integer.parseInt(categoryJSON.get("files").toString()), Integer.parseInt(categoryJSON.get("pages").toString()));
-                categoriesResult.add(category);
                 WikiDB.getInstance().addCategory(category);
             }
         }
-
-        return categoriesResult;
     }
 
     public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
@@ -89,29 +87,34 @@ public class JsonParser {
     public static void parsePagesToDatabase(List<Category> categories) throws IOException {
         int counter = 1;
 
-        for (Category category : categories) {
-            System.out.println("Category - " + counter++);
-            String categoryName = category.getCategoryName().replaceAll(" ", "%20");
-            categoryName = categoryName.replaceAll("&", "%26");
-            String url = "https://en.wikipedia.org/w/api.php?action=query&list=categorymembers&format=json&cmlimit=max&cmtitle=Category:" + categoryName;
-            JSONObject json = readJsonFromUrl(url);
+        for (int k=0; k < categories.size() ; ++k) {
+            try {
+                System.out.println("Category - " + counter++ + "/13000");
 
-            JSONObject query = new JSONObject(json.get("query").toString());
+                String categoryName = categories.get(k).getCategoryName().replaceAll(" ", "%20");
+                categoryName = categoryName.replaceAll("&", "%26");
+                String url = "https://en.wikipedia.org/w/api.php?action=query&list=categorymembers&format=json&cmlimit=max&cmtitle=Category:" + categoryName;
 
-            JSONArray pages = (JSONArray) query.get("categorymembers");
-            Iterator<Object> iterator = pages.iterator();
+                JSONObject json = readJsonFromUrl(url);
+                JSONObject query = new JSONObject(json.get("query").toString());
+                JSONArray pages = (JSONArray) query.get("categorymembers");
 
-            while (iterator.hasNext()) {
+                Iterator<Object> iterator = pages.iterator();
 
-                JSONObject pageJSON = (JSONObject) iterator.next();
-                if (!pageJSON.get("title").toString().contains("Category:")) {
-                    Page page = new Page(pageJSON.get("title").toString(), category);
-                    try {
-                        WikiDB.getInstance().addPage(page);
-                    } catch (HibernateException ex) {
-                        continue;
+                while (iterator.hasNext()) {
+
+                    JSONObject pageJSON = (JSONObject) iterator.next();
+                    if (!pageJSON.get("title").toString().contains("Category:")) {
+                        Page page = new Page(pageJSON.get("title").toString(), categories.get(k));
+                        try {
+                            WikiDB.getInstance().addPage(page);
+                        } catch (HibernateException ex) {
+                            continue;
+                        }
                     }
                 }
+            } catch (UnknownHostException ex) {
+                continue;
             }
         }
     }
--- src/main/java/parser/WikiDB.java
+++ src/main/java/parser/WikiDB.java
@@ -1,22 +1,12 @@
 package parser;
 
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import parser.entities.Category;
-import parser.entities.Page;
-
+import org.hibernate.*;
+import parser.entities.*;
 import javax.persistence.Query;
-import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.nio.file.*;
+import java.util.*;
 
 public class WikiDB {
   private static WikiDB instance = null;
@@ -40,7 +30,6 @@ public class WikiDB {
       tx = session.beginTransaction();
 
       session.save(category);
-
       tx.commit();
     }
   }
@@ -54,7 +43,6 @@ public class WikiDB {
       tx = session.beginTransaction();
 
       session.save(page);
-
       tx.commit();
     } catch (HibernateException ex) {
       throw ex;
@@ -76,7 +64,6 @@ public class WikiDB {
               + "from Category c");
 
       categories =  query.getResultList();
-
       tx.commit();
     }
 
@@ -102,7 +89,6 @@ public class WikiDB {
               + "from Page p");
 
       pages =  query.getResultList();
-
       tx.commit();
     }
 
@@ -115,7 +101,60 @@ public class WikiDB {
 
   public synchronized void saveAllDataToFile(String filePath) throws IOException {
     Path file = Paths.get(filePath);
+    List<String> text = new ArrayList<>();
 
+    List<Category> categories = new ArrayList<>();
+
+    for (long i = 1; i <=  13000 ; i+=500) {
+
+      SessionFactory sessionFactory = HibernateUtilities.getSessionFactory();
+
+      try (Session session = sessionFactory.openSession()) {
+        Transaction tx;
+        tx = session.beginTransaction();
+
+        Query query = session.createQuery("select "
+                + "new parser.entities.Category(c.id, c.categoryName, c.numberOfFiles, c.numberOfPages) "
+                + "from Category c "
+                + "where c.id>=:min and c.id<=:max");
+
+        query.setParameter("min", i);
+        query.setParameter("max", i+499);
+
+        categories =  query.getResultList();
+        tx.commit();
+      }
+
+      for (Category category : categories) {
+        System.out.println("Category - " + category.getId() + "/13000");
+
+        text.add(category.toString());
+
+        SessionFactory sessionFactory2 = HibernateUtilities.getSessionFactory();
+
+        List<Page> pages = new ArrayList<>();
+
+        try (Session session2 = sessionFactory2.openSession()) {
+          Transaction tx2;
+          tx2 = session2.beginTransaction();
+          Query query2 = session2.createQuery("select "
+                  + "new parser.entities.Page(p.id, p.pageName, p.category) "
+                  + "from Page p where p.category.id=:id");
+          query2.setParameter("id", category.getId());
+
+          pages =  query2.getResultList();
+          tx2.commit();
+        }
+
+        for (Page page : pages) {
+          text.add("\t->\t" + page.toStringWithoutCategory());
+        }
+      }
+    }
+    Files.write(file, text, Charset.forName("UTF-8"));
+  }
+
+  public static List<Category> getAllCategories(){
     SessionFactory sessionFactory = HibernateUtilities.getSessionFactory();
 
     List<Category> categories = new ArrayList<>();
@@ -128,35 +167,8 @@ public class WikiDB {
               + "from Category c");
 
       categories =  query.getResultList();
-
       tx.commit();
     }
-
-    List<String> text = new ArrayList<>();
-    for (Category category : categories) {
-
-      text.add(category.toString());
-
-      SessionFactory sessionFactory2 = HibernateUtilities.getSessionFactory();
-
-      List<Page> pages = new ArrayList<>();
-
-      try (Session session = sessionFactory2.openSession()) {
-        Transaction tx;
-        tx = session.beginTransaction();
-        Query query = session.createQuery("select "
-                + "new parser.entities.Page(p.id, p.pageName, p.category) "
-                + "from Page p");
-
-        pages =  query.getResultList();
-
-        tx.commit();
-      }
-
-      for (Page page : pages) {
-        text.add("\t->\t" + page.toStringWithoutCategory());
-      }
-    }
-    Files.write(file, text, Charset.forName("UTF-8"));
+    return categories;
   }
 }
